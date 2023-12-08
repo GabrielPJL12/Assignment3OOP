aa577f256414054f87dcce7e7c8c2c588ed3f852
Gabriel Leclerc <119021693+GabrielPJL12@users.noreply.github.com>
2023-12-08 14:45:36 -0700

    Changed main to WordTracker.java

---
+++ BST/src/driver/WordTracker.java
@@ -0,0 +1,12 @@
+package driver;
+
+import java.util.logging.FileHandler;
+
+import utilities.*;
+
+public class WordTracker {
+    public static void main(String[] args) {
+        String InputFilePath = args[0];
+        
+    }
+}
